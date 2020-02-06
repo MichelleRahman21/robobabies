9b6d789daff1c38f6827ba8aee0327e76b58cddb
michelleRahman21 <michgomez1807@hotmail.com>
2020-02-06 10:05:12 -0500

    Create card component

---
+++ src/card.js
@@ -0,0 +1,14 @@
+import React from 'React'
+
+const Card = ({ name, email, id}) =>{
+  return(
+    <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
+        <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
+          <div>
+          <h2>{name}</h2>
+          <p>{email}</p>
+          </div>
+  );
+}
+
+export default Card;
